7555726611e606c7c4df8af00954ac749e5727d2
Amandeep Singh <amandeep511997@gmail.com>
2017-12-30 20:38:36 +0530

    Add a data structure

--- model.py
+++ model.py
@@ -1,5 +1,27 @@
 from configurations import *
 
-class Model():
+# inherits from dictionary class in python
+class Model(dict):
+	captured_pieces = { 'white': [], 'black': [] }
+	player_turn = None
+	# number of turns played since the last capture or the pawn movement
+	halfmove_clock = 0 
+	# increments after each every black move
+	full_move_number = 1
+	history = []
+
 	def __init__(self):
 		pass
+
+	def get_piece_at(self, position):
+		return self.get(position)
+
+	# returns alphanumeric position of the rowcol given.
+	def get_alphanumeric_position(self, rowcol):
+		if self.is_on_board(rowcol):
+			row, col = rowcol
+			return "{}{}".format(X_AXIS_LABELS[col], Y_AXIS_LABELS[row])
+
+	def is_on_board(self, rowcol):
+		row, col = rowcol
+		return (0 <= row <= 7) and (0 <= col <= 7)
